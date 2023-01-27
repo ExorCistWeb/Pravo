130352570d79cb94b997555291ff75d911f9500b
ExorCistWeb <75250883+ExorCistWeb@users.noreply.github.com>
2023-01-28 02:35:59 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,17 +10,49 @@ window.onscroll = function () {
   }
 };
 
-document.querySelector(".buyer-changer").onclick = (e) => {
-  console.log(e.target.classList);
+const buyerChanger = document.querySelector(".buyer-changer");
+const buyerChangerClass = buyerChanger.classList;
 
-  if (e.target.classList.contains("buyer")) {
-    e.target.classList.add("owner");
-    e.target.classList.remove("buyer");
+const $ = (i) => document.querySelector(i);
+const idItem = (i) => document.getElementById(i);
+
+const checkBuyers = (onClick = false) => {
+  if (onClick) {
+    if (buyerChangerClass.contains("buyer")) {
+      buyerChangerClass.add("owner");
+      buyerChangerClass.remove("buyer");
+
+      idItem("owner-para").classList.add("active");
+      idItem("buyer-para").classList.remove("active");
+      idItem("owner-page").classList.add("active");
+      idItem("buyer-page").classList.remove("active");
+    } else {
+      buyerChangerClass.add("buyer");
+      buyerChangerClass.remove("owner");
+
+      idItem("owner-para").classList.remove("active");
+      idItem("buyer-para").classList.add("active");
+      idItem("owner-page").classList.remove("active");
+      idItem("buyer-page").classList.add("active");
+    }
   } else {
-    e.target.classList.add("buyer");
-    e.target.classList.remove("owner");
+    if (!buyerChangerClass.contains("buyer")) {
+      idItem("owner-para").classList.add("active");
+      idItem("buyer-para").classList.remove("active");
+      idItem("owner-page").classList.add("active");
+      idItem("buyer-page").classList.remove("active");
+    } else {
+      idItem("owner-para").classList.remove("active");
+      idItem("buyer-para").classList.add("active");
+      idItem("owner-page").classList.remove("active");
+      idItem("buyer-page").classList.add("active");
+    }
   }
 };
+
+checkBuyers();
+
+buyerChanger.onclick = (e) => checkBuyers(true);
 
 const swiper = new Swiper(".slider-head", {
   spaceBetween: 50,
@@ -45,4 +77,27 @@ const swiper2 = new Swiper(".slider-body", {
     nextEl: ".body-button-next",
     prevEl: ".body-button-prev",
   },
+});
+
+const checkElementOnScroll = (i, el) => {
+  const element = document.querySelector(i);
+  const position = element.getBoundingClientRect();
+
+  if (position.top < window.innerHeight && position.bottom >= 0) {
+    $(el).classList.add("active");
+  } else {
+    $(el).classList.remove("active");
+  }
+};
+
+window.addEventListener("scroll", function () {
+  checkElementOnScroll(".onblock_land", ".buyer-link_land");
+  checkElementOnScroll(".onblock_start", ".buyer-link_main");
+  checkElementOnScroll(".onblock_documents", ".buyer-link_documents");
+  checkElementOnScroll(".onblock_rent", ".buyer-link_rent");
+
+  checkElementOnScroll(".onblock_start-2", ".buyer-link_main-2");
+  checkElementOnScroll(".onblock_country-side", ".buyer-link_country-side");
+  checkElementOnScroll(".onblock_buildings", ".buyer-link_buildings");
+  checkElementOnScroll(".onblock_map", ".buyer-link_map");
 });
